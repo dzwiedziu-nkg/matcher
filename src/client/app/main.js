a76d02f6c5f1ad0e2cf793292cd3d08aa55be424
nkg <nkg753@gmail.com>
2016-12-20 22:09:24 +0100

    Try to migrate to npm, webpack and react

---
+++ src/client/app/main.js
@@ -0,0 +1,4 @@
+require("bootstrap-webpack");
+import App from './app.jsx';
+import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
+import BootstrapTheme from 'bootstrap/dist/css/bootstrap-theme.min.css';
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'src/client/js');
+var APP_DIR = path.resolve(__dirname, 'src/client/app');
+
+var config = {
+    entry: APP_DIR + '/main.js',
+    output: {
+        path: BUILD_DIR,
+        filename: 'bundle.js'
+    },
+    module : {
+        loaders : [
+            {
+                test: /.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            },
+            {
+                test: /\.css$/,
+                loader: "style-loader!css-loader"
+            },
+            {
+                test: /\.png$/,
+                loader: "url-loader?limit=100000"
+            },
+            {
+                test: /\.jpg$/,
+                loader: "file-loader"
+            },
+            {
+                test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url?limit=10000&mimetype=application/font-woff'
+            },
+            {
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url?limit=10000&mimetype=application/octet-stream'
+            },
+            {
+                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'file'
+            },
+            {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url?limit=10000&mimetype=image/svg+xml'
+            }
+        ]
+    }
+};
+
+module.exports = config;
